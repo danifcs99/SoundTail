937de0752fc4d023a89f816ac58e50c5032c29f8
danifcs99 <danielsantaellarodriguez1999@gmial.com>
2024-06-11 21:31:49 +0200

    todo el tochaco

---
+++ Client/soundtail/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import {Route, Routes } from 'react-router-dom';
+import { Home, Login } from './components';
+
+function App() {
+  return (
+    <div className="w-screen h-screen bg-blue-400 flex justify-center items-center">
+      <Routes>
+        <Route path='/login' element={<Login />} />
+        <Route path='/*' element={<Home />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
